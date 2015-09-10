d97f56888528bd0d7267ecd35ad6c426b2cb051c
adtan <adrian.w.tan@nokia.com>
2015-09-10 15:35:02 +0800

    added mongoose

--- SRAN BBD/app.js
+++ SRAN BBD/app.js
@@ -6,6 +6,19 @@ var app = express();
 // path setup
 app.use(express.static(path.join(__dirname, 'client')));
 
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/hackathonDB');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("DB Connected");
+});
+
+module.exports = mongoose.model('Site', new Schema());
+
+
 // Start server
 var serverPort = 4000;
 app.listen(serverPort, function() {
