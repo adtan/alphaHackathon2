f26180138734eb25c178b8c11a9e4a15f106e37b
unknown <drivera@CNU1311TV6.nsn-intra.net>
2015-09-14 10:42:15 +0800

    added gui

--- SRAN BBD/client/app.js
+++ SRAN BBD/client/app.js
@@ -29,7 +29,7 @@
                 'gsmbbcapacity':{
                     'trxs':24
                 }
-            },   
+            },
             {
                 'smconfiguration':'FSMF+FBBA+FBBA+FSMF+FBBC+FBBC',
                 'sm1': {
@@ -55,5 +55,21 @@
                 }
             }
         ];
+
+      $scope.FsmfCount=1;
+      $scope.array=[1];
+
+      $scope.isLteChecked = true;
+
+      $scope.fsmfAddedClicked = function(){
+       $scope.FsmfCount = $scope.FsmfCount + 1;
+       $scope.array.push($scope.FsmfCount);
+     };
+
+      $scope.fsmfRemoveClicked = function(){
+       $scope.FsmfCount = $scope.FsmfCount - 1;
+       $scope.array.pop();
+     };
+
     }]);
 })();
